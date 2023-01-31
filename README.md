2fee391e212763d404273a055ecd875d9eb62cb0
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-01-31 11:15:01 +0000

    ...

--- README.md
+++ README.md
@@ -1,9 +1,65 @@
-# Master Struct
-Master Struct is a library for creating and managing data structures in TypeScript. 
-But also let's you store metadata about the structure which let's your create ui's for the structure.
-While also letting you stringify and parse the structure to and from JSON by default.
+# What is this?
+Master Validator is a basic runtime type checker for TypeScript.
+Easy to use and easy to extend.
 
-It's an upgrade to the https://github.com/DeepDoge/cute-struct
+# Installation
+```bash
+npm install https://github.com/DeepDoge/master-validator.git
+```
 
-# Note
-Not finished.
+# Usage
+```ts
+import { ms } from 'master-validator/library';
+const { string, number, nullable, object, min, oneOf, union, literal, rangeLength, intersection } = ms
+
+const person = object({
+    name: rangeLength(string, 1, 32),
+    age: nullable(min(number, 0)),
+    sex: union(literal('man'), literal('woman')),
+    // or you can use `oneOf` instead of `union` and `literal`
+    city: nullable(oneOf(
+        'Kraków', 
+        'Oaxaca', 
+        'Moscow', 
+        'Kabul', 
+        'Baghdad', 
+        'Kuala, Lumpur', 
+        'Jeddah', 
+        'Riyadh', 
+        'Mogadishu', 
+        'Dubai', 
+        'Abu Dhabi', 
+        'Sanaa', 
+        'Ibadan', 
+        'Taizz', 
+        'Tehran'
+    )) 
+})
+
+const memberRole = oneOf('admin', 'moderator', 'user')
+const member = intersection(person, object({
+    id: string,
+    role: memberRole
+}))
+```
+
+You can use if statement to check if value is valid<br/>
+if its valid typescript will infer type of value
+```ts
+if (member(unknownValue)) 
+{
+    // So you can use it like this
+    unknownValue.name // string
+}
+```
+
+Or you can use parse function to throw error if value is invalid
+```ts
+const value = ms.parseUnknown(member, unknownValue) // throws error if value is invalid
+// If value is valid typescript will infer type of value
+// Then you can use it like this
+value.name // string 
+```
+
+# Inspired by
+https://github.com/DeepDoge/cute-struct
--- library/index.ts
+++ library/index.ts
@@ -33,6 +33,7 @@ export namespace ms
 
     export const literal = <T extends Any>(value: T) => createValidator((v: unknown): v is T => v === value, (v: unknown) => `Expected ${value?.toString()}, got ${v?.toString()}`)
     export const oneOf = <T extends Any>(...values: T[]) => createValidator((value: unknown): value is T => values.includes(value as T), (value: unknown) => `Expected one of ${values.map(v => v?.toString()).join(", ")}, got ${value?.toString()}`)
+    export const oneOfType = <T extends Validator<any>>(type: T, ...values: TypeOfValidator<T>[]) => createValidator((value: unknown): value is TypeOfValidator<T> => values.includes(value as TypeOfValidator<T>), (value: unknown) => `Expected one of ${values.map(v => type.error(v)).join(", ")}, got ${value?.toString()}`)
 
     export const object = <T extends Obj>(validators: { [K in keyof T]: Validator<T[K]> }) => createValidator((value: unknown): value is T => value !== null && typeof value === "object" && Object.entries(validators).every(([key, validator]) => validator((value as T)[key])), (value: unknown) => `Expected object, got ${typeof value}`)
     export const array = <T extends Validator<any>>(validator: T) => createValidator((value: unknown): value is TypeOfValidator<T>[] => Array.isArray(value) && value.every(v => validator(v)), (value: unknown) => `Expected array, got ${typeof value}`)
@@ -49,15 +50,63 @@ export namespace ms
     export const maxLength = <T extends Validator<any>>(validator: T, max: number) => createValidator((value: unknown): value is TypeOfValidator<T> => validator(value) && value.length <= max, (value: unknown) => `Expected ${validator.error(value)}.length <= ${max}, got ${value}`)
     export const rangeLength = <T extends Validator<any>>(validator: T, min: number, max: number) => createValidator((value: unknown): value is TypeOfValidator<T> => validator(value) && value.length >= min && value.length <= max, (value: unknown) => `Expected ${validator.error(value)}.length >= ${min} && <= ${max}, got ${value}`)
 
-    export function parse<T>(validator: Validator<T>, value: T): T
+    export function parse<T extends Validator<any>>(validator: T, value: TypeOfValidator<T>): TypeOfValidator<T>
     {
         if (validator(value)) return value
-
-        throw new Error("Invalid value")
+        throw new Error(validator.error(value))
     }
 
-    export function parseUnknown<T>(validator: Validator<T>, value: unknown): T
+    export function parseUnknown<T extends Validator<any>>(validator: T, value: unknown): TypeOfValidator<T>
     {
-        return parse(validator, value as T)
+        return parse(validator, value as TypeOfValidator<T>)
     }
 }
+
+// Usage
+const { string, number, nullable, object, min, oneOf, union, literal, rangeLength, intersection } = ms
+
+const person = object({
+    name: rangeLength(string, 1, 32),
+    age: nullable(min(number, 0)),
+    sex: union(literal('man'), literal('woman')),
+    // or you can use `oneOf` instead of `union` and `literal`
+    city: nullable(oneOf(
+        'Kraków', 
+        'Oaxaca', 
+        'Moscow', 
+        'Kabul', 
+        'Baghdad', 
+        'Kuala, Lumpur', 
+        'Jeddah', 
+        'Riyadh', 
+        'Mogadishu', 
+        'Dubai', 
+        'Abu Dhabi', 
+        'Sanaa', 
+        'Ibadan', 
+        'Taizz', 
+        'Tehran'
+    )) 
+})
+
+const memberRole = oneOf('admin', 'moderator', 'user')
+const member = intersection(person, object({
+    id: string,
+    role: memberRole
+}))
+
+const unknownValue: unknown = null
+
+// You can use if statement to check if value is valid
+// if its valid typescript will infer type of value
+if (member(unknownValue)) 
+{
+    // So you can use it like this
+    unknownValue.name // string
+}
+
+// Or you can use parse function to throw error if value is invalid
+const value = ms.parseUnknown(member, unknownValue) // throws error if value is invalid
+// If value is valid typescript will infer type of value
+// Then you can use it like this
+value.name // string
