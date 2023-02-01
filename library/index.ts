91bc33ecb04a2db25458879e0ff33f69177f0ebd
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-02-01 09:16:16 +0000

    ...

--- library/index.ts
+++ library/index.ts
@@ -12,7 +12,10 @@ export namespace mv
     {
         is(value: unknown): value is T
         parse(value: unknown): T
+        typecheck(value: T): T
     }
+
+    export type infer<T> = T extends Validator<infer U> ? U : never
 
     export function createValidator<T>(validator: ValidatorFunction<T>): Validator<T>
     {
@@ -34,6 +37,7 @@ export namespace mv
             validator(value)
             return value as T
         }
+        fn.typecheck = (value: T): T => value
         return fn
     }
 
@@ -57,6 +61,15 @@ export namespace mv
     {
         if (typeof value !== 'bigint') throw new TypeError(`Expected bigint, got ${typeof value}`)
     })
+    export const date = createValidator<Date>((value: unknown) =>
+    {
+        if (!(value instanceof Date)) throw new TypeError(`Expected Date, got ${typeof value}`)
+    })
+    export const never = createValidator<never>((value: unknown) =>
+    {
+        throw new TypeError(`Expected never, got ${typeof value}`)
+    })
+    export const any = createValidator<any>((_: unknown) => { })
 
     export const nullable = <T extends Validator<any>>(validator: T) =>
         createValidator<mv.infer<T> | null>((value: unknown) =>
@@ -69,6 +82,19 @@ export namespace mv
         {
             if (value === undefined) return
             validator(value)
+        })
+    export const partial = <T extends Obj>(obj: T) =>
+        createValidator<Partial<{ [K in keyof T]: mv.infer<T[K]> }>>((value: unknown) =>
+        {
+            if (typeof value !== 'object' || value === null) throw new TypeError(`Expected object, got ${typeof value}`)
+            for (const key in obj)
+            {
+                if (obj.hasOwnProperty(key))
+                {
+                    const validator: any = obj[key]
+                    validator((value as any)[key])
+                }
+            }
         })
 
     export const object = <T extends Obj>(obj: T) =>
@@ -123,31 +149,55 @@ export namespace mv
             }
             throw new TypeError(`Expected one of ${validators.map(v => v.name).join(', ')}, got ${value}`)
         })
+
     export const intersection = <T extends Validator<any>[]>(...validators: T) =>
         createValidator<UnionToIntersection<mv.infer<T[number]>>>((value: unknown) =>
         {
             for (const validator of validators) validator(value)
         })
 
-
-    export const min = <T extends Validator<number>>(validator: T, min: number) =>
+    export const gt = <T extends Validator<number | bigint>>(validator: T, min: number | bigint) =>
+        createValidator<mv.infer<T>>((value: unknown) =>
+        {
+            validator(value)
+            if (value <= min) throw new TypeError(`Expected more than ${min}, got ${value}`)
+        })
+    export const gte = <T extends Validator<number | bigint>>(validator: T, min: number | bigint) =>
         createValidator<mv.infer<T>>((value: unknown) =>
         {
             validator(value)
             if (value < min) throw new TypeError(`Expected ${min} or more, got ${value}`)
         })
-    export const max = <T extends Validator<number>>(validator: T, max: number) =>
+    export const lt = <T extends Validator<number | bigint>>(validator: T, max: number | bigint) =>
+        createValidator<mv.infer<T>>((value: unknown) =>
+        {
+            validator(value)
+            if (value >= max) throw new TypeError(`Expected less than ${max}, got ${value}`)
+        })
+    export const lte = <T extends Validator<number | bigint>>(validator: T, max: number | bigint) =>
         createValidator<mv.infer<T>>((value: unknown) =>
         {
             validator(value)
             if (value > max) throw new TypeError(`Expected ${max} or less, got ${value}`)
         })
-    export const range = <T extends Validator<number>>(validator: T, min: number, max: number) =>
+    export const between = <T extends Validator<number | bigint>>(validator: T, min: number | bigint, max: number | bigint) =>
         createValidator<mv.infer<T>>((value: unknown) =>
         {
             validator(value)
             if (value < min) throw new TypeError(`Expected ${min} or more, got ${value}`)
             if (value > max) throw new TypeError(`Expected ${max} or less, got ${value}`)
+        })
+    export const int = <T extends Validator<number>>(validator: T) =>
+        createValidator<mv.infer<T>>((value: unknown) =>
+        {
+            validator(value)
+            if (!Number.isInteger(value)) throw new TypeError(`Expected integer, got ${value}`)
+        }) 
+    export const finite = <T extends Validator<number>>(validator: T) =>
+        createValidator<mv.infer<T>>((value: unknown) =>
+        {
+            validator(value)
+            if (!Number.isFinite(value)) throw new TypeError(`Expected finite number, got ${value}`)
         })
 
     export const length = <T extends Validator<string>>(validator: T, length: number) =>
@@ -183,7 +233,41 @@ export namespace mv
             if (!pattern.test(value)) throw new TypeError(`Expected pattern of ${pattern}, got ${value}`)
         })
 
-    export type infer<T> = T extends Validator<infer U> ? U : never
+    export const email = <T extends Validator<string>>(validator: T) =>
+        createValidator<`${string[0]}${string}@${string[0]}${string}.${string[0]}${string}`>((value: unknown) =>
+        {
+            validator(value)
+            if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) throw new TypeError(`Expected email, got ${value}`)
+        })
+
+    export const url = <T extends Validator<string>>(validator: T) =>
+        createValidator<`${string[0]}${string}://${string[0]}${string}`>((value: unknown) =>
+        {
+            validator(value)
+            if (!/^[^\s@]+:\/\/[^\s@]+$/.test(value)) throw new TypeError(`Expected url, got ${value}`)
+        })
+
+    export const startsWith = <T extends Validator<string>, P extends string>(validator: T, prefix: P) =>
+        createValidator<`${P}${string}`>((value: unknown) =>
+        {
+            validator(value)
+            if (!value.startsWith(prefix)) throw new TypeError(`Expected starts with ${prefix}, got ${value}`)
+        })
+
+    export const endsWith = <T extends Validator<string>, S extends string>(validator: T, suffix: S) =>
+        createValidator<`${string}${S}`>((value: unknown) =>
+        {
+            validator(value)
+            if (!value.endsWith(suffix)) throw new TypeError(`Expected ends with ${suffix}, got ${value}`)
+        })
+
+    export const dateISO = <T extends Validator<string>>(validator: T) =>
+        createValidator<string>((value: unknown) =>
+        {
+            validator(value)
+            if (!/^\d{4}-\d{2}-\d{2}(T\d{2}:\d{2}:\d{2}(\.\d{1,3})?(Z|([+-]\d{2}:\d{2})))?$/.test(value)) throw new TypeError(`Expected date ISO, got ${value}`)
+        })
+
 
     /*     const test = object({
             0: oneOf("a", "b", "c"),
